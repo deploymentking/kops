5f90caf65d60673635024feb19c6cb86ab19dbd9
Lee Myring <mail@thinkstack.io>
2018-08-30 16:28:50 +0100

    Add helm commands to run,sh

--- 06_helm/run.sh
+++ 06_helm/run.sh
@@ -5,3 +5,11 @@ WORK_DIR=$(cd `dirname $0` && pwd)
 kubectl apply -f ${WORK_DIR}/rbac.yaml
 
 helm init --service-account tiller
+
+helm create node-kops-thnks-tk
+
+helm install node-kops-thnks-tk
+
+helm list
+
+helm history ${NAME_OF_LATEST_DEPLOYED_CHART}
